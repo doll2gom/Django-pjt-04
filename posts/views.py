35fb4d2b8ce2756a4b2242bf0127df966f45fe26
Lightness-of-Being <gje960217@naver.com>
2023-04-19 17:37:09 +0900

    초기세팅2

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def init(request):
+    return redirect('posts:index')
+
+def index(request):
+    return render(request, 'posts/index.html')
