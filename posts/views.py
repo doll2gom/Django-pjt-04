00a0c5cb40abf9c581f3a5eb75f8865fcac14f88
doll2gom <shkim_ok@naver.com>
2023-04-20 16:25:41 +0900

    detail 템플릿

--- posts/views.py
+++ posts/views.py
@@ -11,16 +11,14 @@ def index(request):
     context = {
         'posts': posts
     }
-
     return render(request, 'posts/index.html', context)
+
 
 def detail(request, posts_pk):
     post = Post.objects.get(pk = posts_pk)
-
     context = {
         'post': post,
     }
-
     return render(request, 'posts/detail.html', context)
 
 
@@ -34,16 +32,14 @@ def create(request):
             return redirect('posts:index')
     else:
         form = PostForm()
-    
     context = {
         'form': form,
     }
-
     return render(request, 'posts/create.html', context)
+
 
 def answer(request, post_pk, select_answer):
     post = Post.objects.get(pk=post_pk)
-
     if request.user in post.select1_users.all() or request.user in post.select2_users.all():
         pass
     else:
@@ -51,7 +47,5 @@ def answer(request, post_pk, select_answer):
             post.select1_users.add(request.user)
         elif select_answer == 2:
             post.select2_users.add(request.user)
-
     return redirect('posts:detail', post_pk )
 
-    
