b7b63eaa2e9daa31843a2695260a9556a6823bfa
jeonghwan <ajh5251@gmail.com>
2023-04-20 15:54:11 +0900

    answer 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -14,8 +14,15 @@ def index(request):
 
     return render(request, 'posts/index.html', context)
 
-def detail(request):
-    return
+def detail(request, posts_pk):
+    post = Post.objects.get(pk = posts_pk)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'posts/detail.html', context)
+
 
 def create(request):
     if request.method == "POST":
@@ -34,5 +41,17 @@ def create(request):
 
     return render(request, 'posts/create.html', context)
 
-def answer(request):
-    return
+def answer(request, post_pk, select_answer):
+    post = Post.objects.get(pk=post_pk)
+
+    if request.user in post.select1_users.all() or request.user in post.select2_users.all():
+        pass
+    else:
+        if select_answer == 1:
+            post.select1_users.add(request.user)
+        elif select_answer == 2:
+            post.select2_users.add(request.user)
+
+    return redirect('posts:detail', post_pk )
+
+    
