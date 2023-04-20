d31e546dcb8c84669d98ed9c8371b624ee7d9c3c
Lightness-of-Being <gje960217@naver.com>
2023-04-20 13:46:38 +0900

    urls 추가

--- posts/views.py
+++ posts/views.py
@@ -6,4 +6,14 @@ def init(request):
     return redirect('posts:index')
 
 def index(request):
+    
     return render(request, 'posts/index.html')
+
+def detail(request):
+    return
+
+def create(request):
+    return
+
+def answer(request):
+    return
