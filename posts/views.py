411040fd85e9fdade4dab968790d5c49c10ece95
Lightness-of-Being <gje960217@naver.com>
2023-04-20 14:15:18 +0900

    index 템플릿 구현

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
-
+from .models import Post
 # Create your views here.
 
 def init(request):
     return redirect('posts:index')
 
 def index(request):
-    
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'posts/index.html', context)
 
 def detail(request):
     return
