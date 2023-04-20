c64dd236d9671758dc99c85bed174a0743391b35
Lightness-of-Being <gje960217@naver.com>
2023-04-20 13:31:16 +0900

    로그인 페이지 문제 해결

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,43 +3,28 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Authenti
 from django.contrib.auth import get_user_model
 
 
-# class CustomAuthenticationForm(AuthenticationForm):
-    # username = forms.CharField(
-    #     label='아이디',
-    #     label_suffix='',
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': '아이디 입력',
-    #             'style': 'width: 350px',
-    #         })
-    # )
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='아이디',
+        label_suffix='',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '아이디 입력',
+                'style': 'width: 350px',
+            })
+    )
 
-    # password1 = forms.CharField(
-    #     label='비밀번호',
-    #     label_suffix='',
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': '비밀번호 입력',
-    #             'style': 'width: 350px',
-    #         })
-    # )
-
-
-    # password2 = forms.CharField(
-    #     label='비밀번호 확인',
-    #     label_suffix='',
-    #     widget=forms.PasswordInput(
-    #         attrs={
-    #             'class': 'form-control',
-    #             'placeholder': '비밀번호 재입력',
-    #             'style': 'width: 350px',
-    #         })
-    # )
-    # class Meta(AuthenticationForm.Meta):
-    #     model = get_user_model()
-    #     fields = '__all__'
+    password = forms.CharField(
+        label='비밀번호',
+        label_suffix='',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '비밀번호 입력',
+                'style': 'width: 350px',
+            })
+    )
 
 
 class CustomUserCreationForm(UserCreationForm):
