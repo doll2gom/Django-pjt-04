12b2dac2995e21e4ca0ca1a8afa2b1cb61b70dfd
jeonghwan <ajh5251@gmail.com>
2023-04-21 11:35:22 +0900

    이미지 사이징 재업로드

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill
+from imagekit.processors import ResizeToFill, Thumbnail
 
 # Create your models here.
 class Post(models.Model):
@@ -13,16 +13,16 @@ class Post(models.Model):
     image1 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     image1_thumbnail = ImageSpecField(
         source = 'image1', # 원본 ImageField 명
-        processors = [ResizeToFill(200, 300)], # 처리할 작업목록
-        format = 'JPEG', # 최종 저장 포맷
-        options = {'quality': 60}) # 저장 옵션
+        processors = [ResizeToFill(100, 100)], # 처리할 작업목록
+        format = 'GIF', # 최종 저장 포맷
+        options = {'quality': 100}) # 저장 옵션
 
     select2_content = models.CharField(max_length=50)
     select2_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='select2_users', blank=True)
     image2 = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     image2_thumbnail = ImageSpecField(
         source = 'image2',
-        processors = [ResizeToFill(200, 300)],
-        format = 'JPEG',
-        options = {'quality': 60})
-
+        processors = [Thumbnail(100, 100)],
+        format = 'GIF',
+        options = {'quality': 200})
+    
