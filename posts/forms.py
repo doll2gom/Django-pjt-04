2dce422155031105895e7969cefadf3bb3a1beb2
Lightness-of-Being <gje960217@naver.com>
2023-04-21 12:55:08 +0900

    title 수정

--- posts/forms.py
+++ posts/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'select1_content', 'image1', 'select2_content', 'image2')
+        fields = ('select1_content', 'image1', 'select2_content', 'image2')
